107900f9d306d42bd4e51fc0200a793e389f59ec
Martin Pilat <8pilatmartin8@gmail.com>
2025-01-17 21:11:45 +0100

    fix: use fmt

--- src/python.cpp
+++ src/python.cpp
@@ -98,7 +98,7 @@ std::vector<std::string> get_available_methods() {
     json j;
     std::ifstream f(filename);
     if (!f) {
-        throw FileException("Cannot open file: " + filename);
+        throw FileException(fmt::format("Cannot open file: {}", filename));
     }
 
     f >> j;
